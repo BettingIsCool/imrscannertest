d5ef5409822fcc1bc3b2cc48d5f771964960a19c
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-04-17 00:57:54 +0200

    Update db_imr.py

--- db_imr.py
+++ db_imr.py
@@ -7,7 +7,7 @@ conn = st.connection('imr', type='sql')
 @st.cache_data(ttl=10)
 def get_log(sports: str, leagues: str, min_diff: float, min_limit: float):
 
-  return conn.query(f"SELECT starts, sport_name, league_name, runner_home, runner_away, line, spread_home, spread_away, spread_home_max, diff_home, diff_away, stake_home, stake_away, timestamp, ratings_updated FROM {TABLE_LOG} WHERE starts >= NOW() AND sport_name IN {sports} AND league_name IN {leagues} AND (diff_home >= {min_diff} OR diff_away >= {min_diff}) AND spread_home_max >= {min_limit} ORDER BY starts", ttl=600).to_dict('records')
+  return conn.query(f"SELECT event_id, starts, sport_name, league_name, runner_home, runner_away, line, spread_home, spread_away, spread_home_max, diff_home, diff_away, stake_home, stake_away, timestamp, ratings_updated FROM {TABLE_LOG} WHERE starts >= NOW() AND sport_name IN {sports} AND league_name IN {leagues} AND (diff_home >= {min_diff} OR diff_away >= {min_diff}) AND spread_home_max >= {min_limit} ORDER BY starts", ttl=600).to_dict('records')
 
 
 @st.cache_data(ttl=10)
