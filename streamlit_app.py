9dd7153a62132511112593816fadffb1f87ca0f1
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-04-17 00:31:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,9 +137,11 @@ if authentication_status:
     #st.dataframe(dataframe, column_config={"favorite": st.column_config.CheckboxColumn("Your favorite?", help="Select your **favorite** widgets", default=False,)}, disabled=["widgets"], hide_index=True,)
 
     #st.write(dataframe.style.applymap(highlight_cell, subset=['DIFF HOME', 'DIFF AWAY']).format({'HOME LINE': '{:+g}'.format, 'ODDS HOME': '{:,.3f}'.format, 'ODDS AWAY': '{:,.3f}'.format, 'LIMIT': '{0:g}'.format, 'DIFF HOME': '{:+.0%}'.format, 'DIFF AWAY': '{:+.0%}'.format, 'STAKE HOME': '{}'.format, 'STAKE AWAY': '{}'.format}))
+    
+    dataframe.insert(0, "PROCESSED", [False] * len(dataframe.index), True)
     styled_df = dataframe.style.applymap(highlight_cell, subset=['DIFF HOME', 'DIFF AWAY']).applymap(highlight_outdated_odds, subset=['ODDS UPDATED']).applymap(highlight_outdated_ratings, subset=['RATINGS UPDATED']).format({'HOME LINE': '{:+g}'.format, 'ODDS HOME': '{:,.3f}'.format, 'ODDS AWAY': '{:,.3f}'.format, 'LIMIT': '{0:g}'.format, 'DIFF HOME': '{:+.0%}'.format, 'DIFF AWAY': '{:+.0%}'.format, 'STAKE HOME': '{}'.format, 'STAKE AWAY': '{}'.format})
 
-    st.data_editor(styled_df, num_rows= "dynamic", disabled=True)
+    st.data_editor(styled_df, disabled=True)
 
    
     st.markdown("""👉 Sort rows by clicking on the column header.""")
