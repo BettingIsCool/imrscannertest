6695eec0137196f8d6795ffb5cdc4a4e2743a808
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-04-17 00:27:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,8 +138,8 @@ if authentication_status:
 
     #st.write(dataframe.style.applymap(highlight_cell, subset=['DIFF HOME', 'DIFF AWAY']).format({'HOME LINE': '{:+g}'.format, 'ODDS HOME': '{:,.3f}'.format, 'ODDS AWAY': '{:,.3f}'.format, 'LIMIT': '{0:g}'.format, 'DIFF HOME': '{:+.0%}'.format, 'DIFF AWAY': '{:+.0%}'.format, 'STAKE HOME': '{}'.format, 'STAKE AWAY': '{}'.format}))
     styled_df = dataframe.style.applymap(highlight_cell, subset=['DIFF HOME', 'DIFF AWAY']).applymap(highlight_outdated_odds, subset=['ODDS UPDATED']).applymap(highlight_outdated_ratings, subset=['RATINGS UPDATED']).format({'HOME LINE': '{:+g}'.format, 'ODDS HOME': '{:,.3f}'.format, 'ODDS AWAY': '{:,.3f}'.format, 'LIMIT': '{0:g}'.format, 'DIFF HOME': '{:+.0%}'.format, 'DIFF AWAY': '{:+.0%}'.format, 'STAKE HOME': '{}'.format, 'STAKE AWAY': '{}'.format})
-    styled_data_editor = st.data_editor(styled_df, num_rows= "dynamic")
-    st.write(styled_data_editor)
+
+    st.data_editor(styled_df, num_rows= "dynamic", disabled=True)
 
    
     st.markdown("""👉 Sort rows by clicking on the column header.""")
