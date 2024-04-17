106672536344d8ee8bc207eff6d0f4aa7e06088e
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-04-17 13:43:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
 TABLE_LOG, TABLE_USERS, TABLE_BETS = 'imr_log_test', 'imr_users', 'imr_bets'
+
+
+# Display text om landing page
+TEXT_LANDING_PAGE = """
+
+*by [@BettingIsCool](https://twitter.com/BettingIsCool?lang=en)*
+
+⚠️  **IF YOU ARE LOOKING FOR EASY MONEY, GO ELSEWHERE!**  ⚠️
+
+🔸 The app compares current @Pinnacle odds to my model's fair odds and highlights potential betting opportunities.
+
+🔹 The model is based on proprietary ratings and supports 343 leagues worldwide including NBA, NFL, NHL ⚽️🏒🏀🏈
+
+🔸 The model returned a +3% ROI to @Pinnacle opening prices across tens of thousands of bets over 5 years.
+
+👉 Please visit my website for more information https://bettingiscool.com/impliedmarketratings-web-app/
+
+"""
