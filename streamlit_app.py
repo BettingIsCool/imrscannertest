268ae980bd9f861988973c742fb828d062f66550
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-04-17 13:44:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import streamlit_authenticator as stauth
 
+from config import TEXT_LANDING_PAGE
+
 st.set_page_config(layout="wide")
 
 users = db_imr.get_users()
@@ -25,22 +27,7 @@ placeholder1 = st.empty()
 placeholder2 = st.empty()
 
 placeholder1.title('Implied Market Ratings')
-
-placeholder2.markdown("""
-
-*by [@BettingIsCool](https://twitter.com/BettingIsCool?lang=en)*
-
-⚠️  **IF YOU ARE LOOKING FOR EASY MONEY, GO ELSEWHERE!**  ⚠️
-
-🔸 The app compares current @Pinnacle odds to my model's fair odds and highlights potential betting opportunities.
-
-🔹 The model is based on proprietary ratings and supports 343 leagues worldwide including NBA, NFL, NHL ⚽️🏒🏀🏈
-
-🔸 The model returned a +3% ROI to @Pinnacle opening prices across tens of thousands of bets over 5 years.
-
-👉 Please visit my website for more information https://bettingiscool.com/impliedmarketratings-web-app/
-
-""")
+placeholder2.markdown(TEXT_LANDING_PAGE)
 
 if authentication_status is False:
   st.error("Username/password is incorrect")
